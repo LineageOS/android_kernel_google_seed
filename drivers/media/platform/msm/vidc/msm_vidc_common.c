d4ec5d96f19bf3d2a46ef5d4f439633861a92fa6
Balamurugan Alagarsamy <balaga@codeaurora.org>
2015-12-03 15:29:51 +0530

    msm: vidc: remove sync_lock between get & set session property
    Holding sync_lock for the session_get_property and
    session_set_property is leading to stability issues
    and does not seems to be relevant to hold a sync_lock
    here. So, removing it.
    
    CRs-Fixed: 946713
    Change-Id: I2333829c7d1938ba7b4a45de38398cfb6cdd36a1
    Signed-off-by: Balamurugan Alagarsamy <balaga@codeaurora.org>

--- drivers/media/platform/msm/vidc/msm_vidc_common.c
+++ drivers/media/platform/msm/vidc/msm_vidc_common.c
@@ -3554,7 +3554,6 @@ int msm_comm_try_get_prop(struct msm_vidc_inst *inst, enum hal_property ptype,
 		return -EAGAIN;
 	}
 	hdev = inst->core->device;
-	mutex_lock(&inst->sync_lock);
 	if (inst->state < MSM_VIDC_OPEN_DONE || inst->state >= MSM_VIDC_CLOSE) {
 		dprintk(VIDC_ERR,
 			"%s Not in proper state\n", __func__);
@@ -3617,7 +3616,6 @@ int msm_comm_try_get_prop(struct msm_vidc_inst *inst, enum hal_property ptype,
 	}
 	mutex_unlock(&inst->pending_getpropq.lock);
 exit:
-	mutex_unlock(&inst->sync_lock);
 	return rc;
 }
 
@@ -3894,7 +3892,6 @@ int msm_comm_try_set_prop(struct msm_vidc_inst *inst,
 	}
 	hdev = inst->core->device;
 
-	mutex_lock(&inst->sync_lock);
 	if (inst->state < MSM_VIDC_OPEN_DONE || inst->state >= MSM_VIDC_CLOSE) {
 		dprintk(VIDC_ERR, "Not in proper state to set property\n");
 		rc = -EAGAIN;
@@ -3905,7 +3902,6 @@ int msm_comm_try_set_prop(struct msm_vidc_inst *inst,
 	if (rc)
 		dprintk(VIDC_ERR, "Failed to set hal property for framesize\n");
 exit:
-	mutex_unlock(&inst->sync_lock);
 	return rc;
 }
 
